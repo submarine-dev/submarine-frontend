d3d0c0d2973982c17745fe9c98a975dd4dee7b51
Yuma Satake <satous.y@icloud.com>
2024-03-10 00:58:28 +0900

    fix: RSCで取得する形に修正

--- src/app/(protectedroutes)/home/components/HomeContainer.tsx
+++ src/app/(protectedroutes)/home/components/HomeContainer.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react';
 import HomeContent from './HomeContent';
+import { axiosFn } from '@/lib/axiosFn';
 
 /**
  * ホーム/container
  */
-const HomeContainer: FC = () => {
-  return <HomeContent />;
+const HomeContainer: FC = async () => {
+  const subscriptionsRes = await axiosFn.get(
+    '/subscriptions?results=10'
+  );
+  const listOfSubscriptions =
+    subscriptionsRes.data.subscriptionMaster;
+
+  return (
+    <HomeContent
+      listOfSubscriptions={listOfSubscriptions}
+    />
+  );
 };
 
 export default HomeContainer;
--- src/app/(protectedroutes)/home/components/HomeContent.tsx
+++ src/app/(protectedroutes)/home/components/HomeContent.tsx
@@ -8,13 +8,17 @@ import { Body2Typo } from '@/components/Typography';
 import { SubscriptionBaseType } from '@/types/SubscriptionBaseType';
 import { axiosFn } from '@/lib/axiosFn';
 
+type Props = {
+  listOfSubscriptions: SubscriptionBaseType[];
+};
+
 /**
  * ログイントップ/content
  */
-const HomeContent: FC = () => {
+const HomeContent: FC<Props> = ({
+  listOfSubscriptions,
+}) => {
   const router = useRouter();
-  const [listOfSubscriptions, setListOfSubscriptions] =
-    useState<SubscriptionBaseType[]>([]);
 
   /**
    * サブスク検索ボタンクリック
@@ -33,20 +37,6 @@ const HomeContent: FC = () => {
   ): void => {
     router.push(`/detail/${subscriptionId}`);
   };
-
-  /**
-   * 初回実行
-   * - サブスクリストを取得
-   */
-  useEffect(() => {
-    (async () => {
-      const res = await axiosFn.get(
-        '/subscriptions?results=10'
-      );
-      const newSubscriptions = res.data.subscriptionMaster;
-      setListOfSubscriptions(newSubscriptions);
-    })();
-  }, []);
 
   return (
     <div className="space-y-3">
